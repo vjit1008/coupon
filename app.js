c3d28af247f04c2f1fe7c8b9f210458966f8eb85
vishvajit gadakari <gadakariv@gmail.com>
2023-02-16 14:16:22 +0530

    coupon

---
+++ app.js
@@ -0,0 +1,64 @@
+const form = document.getElementById("form1");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const dice = document.getElementById("dice");
+const image4 = document.getElementById("image4");
+const username = document.getElementById("username");
+const sum = document.getElementById("sum");
+const coupon = document.getElementById("coupon");
+const generateCoupon = document.getElementById("generate-coupon");
+
+let attempts = 0;
+let values = [];
+
+image1.addEventListener("click", function() {
+  form.classList.remove("hidden");
+  image1.style.pointerEvents = "none";
+});
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  let name = form.elements[0].value;
+  let user = form.elements[1].value;
+  username.textContent = `Name: ${name} | Username: ${user}`;
+//   username.style.color=white
+  form.classList.add("hidden");
+  username.classList.remove("hidden");
+  image2.style.pointerEvents = "none";
+  
+});
+
+dice.addEventListener("click", function() {
+  if (attempts === 2) {
+    sum.textContent = "Bad Luck";
+    sum.classList.remove("hidden");
+    dice.style.pointerEvents = "none";
+    return;
+  }
+  const value = Math.floor(Math.random() * 6) + 1;
+  values.push(value);
+  let total = values.reduce((a, b) => a + b, 0);
+  if (values.length === 3) {
+    if (total > 10) {
+      sum.textContent = `Sum: ${total}`;
+      sum.classList.remove("hidden");
+      image4.style.pointerEvents = "auto";
+    } else {
+      sum.textContent = `Try Again after scoring more than 10. Sum: ${total}`;
+      sum.classList.remove("hidden");
+      attempts += 1;
+      values = [];
+    }
+  }
+});
+
+image4.addEventListener("click", function() {
+  coupon.textContent = "Your coupon: " + Math.random().toString(36).substr(2, 12);
+  coupon.classList.remove("hidden");
+  generateCoupon.classList.remove("hidden");
+  image4.style.pointerEvents = "none";
+});
+
+generateCoupon.addEventListener("click", function() {
+  coupon.textContent = "Your coupon: " + Math.random().toString(36).substr(2, 12);
+});
